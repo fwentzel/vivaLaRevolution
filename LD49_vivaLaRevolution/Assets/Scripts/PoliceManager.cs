d3434c03982061f6ecd38fb8b604a10a98d69a15
wentzler <felixwentzel@gmail.com>
2021-10-02 22:07:19 +0200

    Police Group respawning and advancing

--- LD49_vivaLaRevolution/Assets/Scripts/PoliceManager.cs
+++ LD49_vivaLaRevolution/Assets/Scripts/PoliceManager.cs
@@ -7,6 +7,8 @@ public class PoliceManager : MonoBehaviour
     public GameObject policePrefab;
     public HoldPoint spawnPoint;
     public List<PoliceGroup> groups;
+    public float respawnInterval=5;
+    private float nextRespawn=5;
     public static PoliceManager instance { get; private set; }
 
     private void Awake()
@@ -15,23 +17,36 @@ public class PoliceManager : MonoBehaviour
             instance = this;
         else
             Destroy(this);
+nextRespawn=Time.time+respawnInterval;
+        
+    }
+    private void Start() {
+        
         FillGroups();
     }
 
-    private void FillGroups()
+    private void Update() {
+        if(nextRespawn<Time.time){
+            FillGroups(Random.Range(0,3));
+            nextRespawn=Time.time+respawnInterval;
+        }
+    }
+
+    private void FillGroups(int amountPerGroup=-1)
     {
 
         groups = new List<PoliceGroup>(FindObjectsOfType<PoliceGroup>());
         foreach (PoliceGroup group in groups)
         {
-            for (int i = 0; i < group.startSize; i++)
+            int v = amountPerGroup <0?group.startSize:amountPerGroup;
+            for (int i = 0; i < v; i++)
             {
                 GameObject obj = Instantiate(policePrefab, spawnPoint.transform.position, Quaternion.identity);
                 Police police = obj.GetComponent<Police>();
                 group.members.Add(police);
                 police.group = group;
             }
-            group.SetHoldPositionToNext();
+            group.SetHoldPosition();
         }
     }
 

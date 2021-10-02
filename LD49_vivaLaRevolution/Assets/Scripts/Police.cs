38770f6944316ddaed9a8741dd52a57656d46497
wentzler <felixwentzel@gmail.com>
2021-10-02 13:30:21 +0200

    Holdposition and fightwithin range

---
+++ LD49_vivaLaRevolution/Assets/Scripts/Police.cs
@@ -0,0 +1,49 @@
+﻿
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Police : RTSUnit
+{
+
+    public Transform holdPosition;
+    protected override void Start()
+    {
+        moveToPosition = holdPosition.position;
+        base.Start();
+    }
+
+    private void SetHoldPosition(Transform newPos)
+    {
+        holdPosition = newPos;
+        moveToPosition = newPos.position;
+    }
+    protected override void Update()
+    {
+        base.Update();
+        if (targetHealth == null)
+        {
+            foreach (Collider collider in Physics.OverlapSphere(transform.position, detectRadius))
+            {
+                if (collider.tag.Equals("Protestor"))
+                {
+                    targetHealth = collider.GetComponent<Health>();
+                    break;
+                }
+            }
+
+            navMeshAgent.destination = holdPosition == null ? moveToPosition : holdPosition.position;
+        }
+    }
+
+
+
+    private void OnDrawGizmos()
+    {
+ Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, detectRadius);
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(holdPosition.position, fightWithinRange);
+    }
+}
---
+++ LD49_vivaLaRevolution/Assets/Scripts/Protestor.cs
@@ -0,0 +1,35 @@
+﻿
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Protestor : RTSUnit
+{
+    protected override void Update()
+    {
+        base.Update();
+        if(targetHealth==null)
+        {
+            foreach (Collider collider in Physics.OverlapSphere(transform.position, detectRadius))
+            {
+                if (collider.tag.Equals("Police"))
+                {
+                    targetHealth = collider.GetComponent<Health>();
+                    break;
+                }
+            }
+            if (moveToPosition != Vector3.zero)
+                navMeshAgent.destination = moveToPosition;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+ Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, detectRadius);
+        Gizmos.color=Color.green;
+        Gizmos.DrawWireSphere(moveToPosition, fightWithinRange);
+       
+    }
+}

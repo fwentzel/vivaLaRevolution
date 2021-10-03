7d79197a0b7d41cfda91c247b6e02f8725852cd8
Felix Voigtländer <voigtlaender.felix01@googlemail.com>
2021-10-03 14:04:35 +0200

    Changed Building Layer.

--- LD49_vivaLaRevolution/Assets/Scripts/Item.cs
+++ LD49_vivaLaRevolution/Assets/Scripts/Item.cs
@@ -13,13 +13,29 @@ public class Item : MonoBehaviour
 
     public LayerMask obstacleLayer;
 
+    private float maxDistance = 50;
+
     public virtual void Use(Vector3 position)
     {
         if(!transform)
             return;
         
+        // Correct for Distance
+        Vector3 difference = position - transform.position;
+        if (difference.magnitude > maxDistance)
+        {
+            position = transform.position + difference.normalized * maxDistance;
+        }
         
-        
+        //Correct for Hits
+        RaycastHit hit;
+        difference = position - transform.position;
+        if (Physics.Raycast(transform.position, difference, out hit, difference.magnitude, obstacleLayer))
+        {
+            position = hit.point;
+        }
+
+
         
         transform.parent = null;
         
